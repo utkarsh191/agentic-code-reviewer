f0d6206179fdd9c40866d2ba486e4baec3c933bb
UTKARSH KESHARWANI <utkarsh98399426@gmail.com>
2026-09-08 22:32:45 +0530

    chore: initialize project

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Health check
+app.get("/api/health", (_req, res) => {
+  res.json({
+    success: true,
+    message: "Server is running",
+  });
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
