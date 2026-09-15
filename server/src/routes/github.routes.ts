f8db0ba43bf0437703538553846850abe22320cc
UTKARSH KESHARWANI <utkarsh98399426@gmail.com>
2026-09-15 20:14:53 +0530

    feat: add GitHub repositories route

--- server/src/routes/github.routes.ts
+++ server/src/routes/github.routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   githubLogin,
   githubCallback,
+  getRepositories,
 } from "../controllers/github.controller.js";
 
 const router = Router();
 
 router.get("/login", githubLogin);
 router.get("/callback", githubCallback);
+router.get("/repositories", getRepositories);
 
 export default router;
