5429c9799623775370913fe435ad6425918a8828
UTKARSH KESHARWANI <utkarsh98399426@gmail.com>
2026-09-17 15:06:56 +0530

    feat: connect pull request details to selected PR

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,7 @@ import PullRequestDetails from "./pages/PullRequestDetails";
 
 function App() {
   return (
-    <PullRequestDetails
-      onBack={() => {
-        window.history.back();
-      }}
-    />
+    <PullRequestDetails />
   );
 }
 
